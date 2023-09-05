71afebb191901345ad2aa89e54f89796f3bec77e
ZurielAF <143724825+ZurielAF@users.noreply.github.com>
2023-09-05 08:46:26 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author labcca
+ */
+public class ListaTareas {
+    public static void main(String[] args) {
+        String Tarea1 = "Investigar conceptos ";
+        String Tarea2 = "Elaborar un algoritmo ";
+        String Tarea3 = "Realizar un mapa mental ";
+        String Tarea4 = "Comprar un libro ";
+        String Tarea5 = "Realizar ejercicios matematicos ";
+        
+        System.out.println("La primer tarea es: " + Tarea1);
+        System.out.println("La segunda tarea es: " + Tarea2);
+        System.out.println("La tercer tarea es: " + Tarea3);
+        System.out.println("La cuarta tarea es: " + Tarea4);
+        System.out.println("La quinta tarea es: " + Tarea5);
+    }
+    
+}
