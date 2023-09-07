5bfc3889c37a136ef2b5908527d677495bcd34fd
ZurielAF <143724825+ZurielAF@users.noreply.github.com>
2023-09-07 08:42:57 -0600

    Add files via upload

---
+++ AreaRectangulo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author labcca
+ */
+public class AreaRectangulo {
+    public static void main(String[] args) {
+        
+            String nombre = "Zuriel Alcantara Fernandez";
+           int base = 14;
+           int altura = 13;
+           
+           int areaRectangulo = base * altura;
+           
+           System.out.println("Mi nombre es " + nombre);
+        System.out.println(" Y el area del rectuangulo es " + areaRectangulo);
+    }
+  
+}
